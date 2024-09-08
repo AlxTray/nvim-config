c01e5a4ca96baa1a479be8e427282d9fa65a0b6b
AlxTray <alex.ashby02@icloud.com>
2024-09-08 14:51:15 +0100

    feat: removed eyliner and added gruvbox theme

---
+++ nvim/plugin/gruvbox.lua
@@ -0,0 +1,11 @@
+if vim.g.did_load_gruvbox_plugin then
+  return
+end
+vim.g.did_load_gruvbox_plugin = true
+
+vim.o.background = "dark"
+require("gruvbox").setup({
+  terminal_colors = true,
+  transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
